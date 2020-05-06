54e361b69a775f6034ba07d18bbfb42284ae8b3b
Stephan Nunhofer <stephan.nunhofer@t-online.de>
2020-05-06 12:28:49 +0200

    Final test api skript for ms graph
    Signed-off-by: Stephan Nunhofer <stephan.nunhofer@t-online.de>

--- src/notes/msGraph/msGraphAPI.py
+++ src/notes/msGraph/msGraphAPI.py
@@ -28,19 +28,23 @@ def extractCode():
     global answer
     answer = answer[11:48]
     print(answer)
-    
+
+##################################################################################################
+#logging in  
 print("logging in")
 client = Client('07ce1641-3699-492a-ac5d-901b8309bfc0', 'sNCs_0@11N]/ocLdc2S/2sv_bi6xS/hg', account_type='by defect common', office365=True)
-scope = {'offline_access', 'user.read', 'notes.read'}
+scope = {'offline_access', 'user.read', 'notes.read', 'notes.readwrite'}
 url = client.authorization_url('http://localhost:5000', scope, state=None)
 print(url)
-#url = 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=07ce1641-3699-492a-ac5d-901b8309bfc0&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000&response_mode=query&scope=offline_access%20user.read%20mail.read&state=12345'
 
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#acquire authentication code from azure ad
 webbrowser.open_new(url)
 getAnwser()
-
 extractCode()
 
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#exchange code for authentication token and give it to the client class
 token = client.exchange_code('http://localhost:5000', answer)
 #accessToken = token["access_token"]
 #print(token)
@@ -50,22 +54,65 @@ client.token = token
 #print(client.token)
 #print(client.office365_token)
 
-#params = {'Authorization':'Bearer '+accessToken,
-          #'Host':'https://graph.microsoft.com/v1.0/me'}
-#me =  requests.get("https://graph.microsoft.com/v1.0/me", params)
-#print(me.url)
-#print(me)
-#print(token)
-#me = client.get_me()
-#me = client.get_message(message_id="")
 print("logged in")
 
+##################################################################################################
+#get notebook information
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#get list of notebooks
 print("listing notebooks")
 notebooks = client.list_notebooks()
-print (notebooks)
+print("-----------------------------------------------------------")
+print("notebooks:")
+#print(notebooks)
+for key in notebooks:
+    print(key)
+    if isinstance(notebooks[key], list):
+        for item in notebooks[key]:
+            for keyIntern in item:
+                print(keyIntern, ": ", item[keyIntern])
+            print()
+    else:
+        print(notebooks[key])
+    print()
+print("-----------------------------------------------------------")
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#get list of sections
 section_notebook = client.get_notebook_sections('0-84C461DF521C020F!111')
-print(section_notebook)
+print("-----------------------------------------------------------")
+print("sections:")
+for key in section_notebook:
+    print(key)
+    if isinstance(section_notebook[key], list):
+        for item in section_notebook[key]:
+            for keyIntern in item:
+                print(keyIntern, ": ", item[keyIntern])
+            print()
+    else:
+        print(section_notebook[key])
+    print()
+print("-----------------------------------------------------------")
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#get list of pages with content
 pages = client.list_pages()
-print(pages)
+print("-----------------------------------------------------------")
+print("Pages:")
+for key in pages:
+    print(key)
+    if isinstance(pages[key], list):
+        for item in pages[key]:
+            for keyIntern in item:
+                if(keyIntern == 'contentUrl'):
+                    print(keyIntern, ": ", client._get(item[keyIntern]))
+                else:
+                    print(keyIntern, ": ", item[keyIntern])
+            print()
+    else:
+        print(pages[key])
+    print()
+print("-----------------------------------------------------------")
 
 print("listed notebooks")
