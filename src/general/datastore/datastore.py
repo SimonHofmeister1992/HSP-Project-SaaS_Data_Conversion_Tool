71b0f72a739b1501f2d7fc7a1a91306733c7d938
hos47096 <simon1.hofmeister@st.oth-regensburg.de>
2020-05-22 22:37:43 +0200

    implemented interface of datastore

---
+++ src/general/datastore/datastore.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append("../../general/")
+import dataObject as dataObject
+
+"""interface to persist and request persisted objects"""
+class datastore:
+    def persist(self, dataObject):
+        """abstract function to persist data objects in a concrete datastore"""
+        return     
+
+    def get(self, dataObject):
+        """abstract function to get persisted objects in a concrete datastore. Returns a list of dataObjects"""
+        return
